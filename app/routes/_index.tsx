0654277cefd1ccbc05976a581e0ae0e60877cc7d
Amal Bouazza <amal.bouazza@u-virtuelle.tn>
2025-03-03 00:51:56 +0100

    commit login test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Form, useActionData, useNavigate } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,7 +9,29 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return ( <div></div>
+  return ( 
+  
+  <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600">
+    <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold text-center text-gray-800">Login</h1>
+      <form method="post" className="space-y-6 mt-6">
+        <div>
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
+          <input type="text" id="name" className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"name="name" required />
+        </div>
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+          <input type="email" id="email" className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700">PassWord</label>
+          <input type="password" id="password"  className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" name="password" required />
+        </div>
+        <button type="submit" className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Submit</button>
+
+      </form>
+    </div>
+  </div>
    
   );
 }
